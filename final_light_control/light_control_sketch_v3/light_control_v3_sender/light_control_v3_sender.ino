eacaa76c890a61f2a12cbf1ae05846741ad0ebec
frankwzhg <frankwzhg@hotmail.com>
2015-11-23 16:28:30 +0800

    nrf2401

---
+++ final_light_control/light_control_sketch_v3/light_control_v3_sender/light_control_v3_sender.ino
@@ -0,0 +1,46 @@
+#include <nRF24L01.h>
+#include <Mirf.h>
+#include <MirfHardwareSpiDriver.h>
+#include <MirfSpiDriver.h>
+
+int incoming[2];
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  Mirf.cePin = 9;
+  Mirf.csnPin = 10;
+  Mirf.spi = &MirfHardwareSpi;
+  Mirf.init();
+
+  Mirf.setRADDR((byte *)"Send01");
+  Mirf.payload = sizeof(unsigned int);
+
+  Mirf.channel = 3;
+  Mirf.config();
+
+  Serial.println("I'm Sender ...");
+
+}
+unsigned int adata = 0;
+void loop() {
+  // put your main code here, to run repeatedly:
+  if(Serial.available()>=2){
+    //fill array by python
+        for( int i = 0; i<2; i++){
+          incoming[i] = Serial.read();
+           };
+  }
+  adata = incoming[0];
+  byte data[Mirf.payload];
+
+  data[0] = adata & 0xFF;
+  data[1] = adata >> 8;
+
+  Mirf.setTADDR((byte *)"Rec01");
+  Mirf.send(data);
+  while(Mirf.isSending()){}
+  delay(20);
+
+}
